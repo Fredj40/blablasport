7ebf9b2aeea39006a72f2e6006c38ecfb3362359
frederic jeanne <fredericjeanne@gmail.com>
2023-02-14 23:58:51 +0100

    fred

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,17 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+  against:
+
+  [ :first_name, :last_name ],
+  associated_against: {
+    sport: [:sport_name]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
+
 end
